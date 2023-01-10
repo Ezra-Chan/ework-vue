77e21b520aef2e709289396e7e886a7f86fae3aa
chenxin <chenxin@njsdata.com>
2023-01-10 18:58:50 +0800

    feat: secondary packaging axios
    Change-Id: Ib88c10e350472a4d877f30cc9f026b2712537908

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,43 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import type { ConfigEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 import * as path from "path";
 
-export default defineConfig({
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
+export default defineConfig(({ mode }: ConfigEnv) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
+      extensions: [".js", ".json", ".ts", ".vue"],
     },
-  },
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-      imports: ["vue", "vue-router"],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
-  server: {
-    // port: 8080,
-    // hmr: {
-    //   host: "127.0.0.1",
-    //   port: 8080,
-    // },
-    proxy: {
-      "/api": {
-        target: "your https address",
-        changeOrigin: true,
-        rewrite: (path: string) => path.replace(/^\/api/, ""),
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+        imports: ["vue", "vue-router"],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
+    server: {
+      // port: 8080,
+      // hmr: {
+      //   host: "127.0.0.1",
+      //   port: 8080,
+      // },
+      proxy: {
+        "/api": {
+          target: env.VITE_APP_API_BASE_URL,
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
